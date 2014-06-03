139db6c7c058377db4724bb386756e235043007f
Jerome Lachaud <jerome.lachaud@gmail.com>
2014-06-03 15:56:45 +0200

    healthy refactoring

---
+++ app.php
@@ -0,0 +1,21 @@
+	<?php
+	$data = $conn->query('SELECT slug FROM feed ORDER BY nom ASC');
+	$feed = $data->fetchAll(PDO::FETCH_COLUMN, 0);
+
+	foreach ($feed as $feedURL):
+		$rss = simplexml_load_file($feedURL);
+		$title = $rss->channel->title;
+				echo '<div class="channel">';
+				echo'<span class="channel-title"><a href="'.$rss->channel->link.'"><h2>'.$rss->channel->title.'</h2></a></span>';
+					$i = 0;
+					foreach ($rss->channel->item as $item) {
+						if($i == $items) break;
+							echo '<p class="news">
+									<span class="news"><a href="'.$item->link.'">'.$item->title.'</a></span>
+					        </p>';
+				        $i++;
+					}
+				echo '</div>';
+		unset($feedURL);
+	endforeach;
+	?>
